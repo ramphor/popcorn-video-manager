2bf2100a68d61f3d3fe174c86668a2d919775f38
Puleeno Nguyen <puleeno@gmail.com>
2020-03-27 06:15:05 +0700

    define plugin info

---
+++ popcorn.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: Ramphor Popcorn
+ * Plugin URI: https://puleeno.com
+ * Author: Ramphor Premium
+ * Author URI: https://puleeno.com
+ * Version: 1.0.0
+ * Description: The video manager for WordPress. Managa, play and share your videos.
+ */
+
+define( 'POPCORN_PLUGIN_FILE', __FILE__ );
