a85b4f6d336c26f43f21386c5f111712c1b7d72e
Puleeno Nguyen <puleeno@gmail.com>
2021-03-08 04:06:06 +0700

    Define new plugin structure

--- includes/class-ramphor-popcorn-admin.php
+++ includes/class-ramphor-popcorn-admin.php
@@ -1,14 +1,17 @@
 <?php
 
-class Ramphor_Popcorn_Admin {
-	public function __construct() {
-		$this->includes();
-	}
+class Ramphor_Popcorn_Admin
+{
+    public function __construct()
+    {
+        $this->includes();
+    }
 
-	public function includes() {
-		require_once dirname( __FILE__ ) . '/admin/class-ramphor-popcorn-admin-menus.php';
-		require_once dirname( __FILE__ ) . '/admin/class-ramphor-popcorn-admin-post-list.php';
-	}
+    public function includes()
+    {
+        require_once dirname(__FILE__) . '/admin/class-ramphor-popcorn-admin-menus.php';
+        require_once dirname(__FILE__) . '/admin/class-ramphor-popcorn-admin-post-list.php';
+    }
 }
 
 new Ramphor_Popcorn_Admin();
