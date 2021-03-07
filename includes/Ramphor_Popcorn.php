721b83ab86016e1aae5b5513a9fc5782f82a354c
Puleeno Nguyen <puleeno@gmail.com>
2021-03-08 04:27:38 +0700

    Change plugin to new structure and follow coding standard PSR2

---
+++ includes/Ramphor_Popcorn.php
@@ -0,0 +1,34 @@
+<?php
+
+class Ramphor_Popcorn
+{
+    protected static $instance;
+
+    public static function get_instance()
+    {
+        if (is_null(self::$instance)) {
+            self::$instance = new self();
+        }
+
+        return self::$instance;
+    }
+
+    private function __construct()
+    {
+        $this->bootstrap();
+    }
+
+    public function bootstrap()
+    {
+    }
+
+    protected function is_request($request)
+    {
+        switch ($request) {
+            case 'admin':
+                return is_admin();
+            case 'cron':
+                return defined('DOING_AJAX') && DOING_AJAX;
+        }
+    }
+}
--- ramphor-popcorn.php
+++ ramphor-popcorn.php
@@ -11,7 +11,13 @@
 define( 'POPCORN_PLUGIN_FILE', __FILE__ );
 
 if ( ! class_exists( 'Ramphor_Popcorn' ) ) {
-	require_once dirname( __FILE__ ) . '/includes/class-ramphor-popcorn.php';
+	$composerAutoload = sprintf('%s/vendor/autoload.php', dirname(POPCORN_PLUGIN_FILE));
+
+	if (!file_exists($composerAutoload)) {
+		error_log('Plugin Ramphor Popcorn is not loaded');
+		return;
+	}
+	require_once $composerAutoload;
 }
 
 if ( ! function_exists( 'ramphor_popcorn' ) ) {
