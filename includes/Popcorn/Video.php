73ed89c7e3e3d9466755866fac1a372dbc9847e0
Puleeno Nguyen <puleeno@gmail.com>
2022-12-10 11:07:35 +0700

    Install wallary to create video gallery

--- includes/Popcorn/Video.php
+++ includes/Popcorn/Video.php
@@ -5,10 +5,23 @@ class Video
 {
     protected $post;
 
+    protected $videoId;
+
+    protected $tile;
+
+    protected $thumbnailId;
+
+    protected $embedCode;
+
+    protected $previewVideo;
+
+    protected $galleryId;
+
     /**
      * @param   \WP_Post|int  $post The post or post ID
      */
     public static function createFromPost($post)
     {
+
     }
 }
--- includes/Ramphor_Popcorn.php
+++ includes/Ramphor_Popcorn.php
@@ -4,12 +4,19 @@ use Ramphor\Popcorn\Admin\Admin;
 use Ramphor\Popcorn\Installer;
 use Ramphor\Popcorn\PlatformManager;
 use Ramphor\Popcorn\Video;
+use Ramphor\Wallery\Factory\MetaboxFactory;
+use Ramphor\Wallery\Wallery;
 
 class Ramphor_Popcorn
 {
     const POST_TYPE = 'video';
 
     protected static $instance;
+
+    /**
+     * @var \Ramphor\Wallery\Wallery
+     */
+    protected $walleryInstance;
 
     public static function get_instance()
     {
@@ -29,6 +36,12 @@ class Ramphor_Popcorn
 
     public function bootstrap()
     {
+        if ( class_exists( Wallery::class ) ) {
+            $walleryFactory  = new MetaboxFactory('Image Heading Text');
+            $this->walleryInstance = new Wallery( $walleryFactory );
+
+            $this->walleryInstance->setId( 'popcorn_video_gallary' );
+        }
     }
 
     protected function is_request($request)
@@ -55,5 +68,17 @@ class Ramphor_Popcorn
         register_activation_hook(POPCORN_PLUGIN_FILE, [Installer::class, 'active']);
 
         add_action('the_post', [Video::class, 'createFromPost']);
+
+        add_action('add_meta_boxes', [$this, 'wallery_register_metabox']);
+
+    }
+
+    function wallery_register_metabox() {
+        add_meta_box(
+            'metabox_id',
+            __('Video Gallery', 'ramphor_popcorn'),
+            array( $this->walleryInstance, 'render' ),
+            static::POST_TYPE
+        );
     }
 }
