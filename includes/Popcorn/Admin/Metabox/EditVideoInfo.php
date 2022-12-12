e2945117b8815a4a6e63eedf4bc2bacd36fba27c
Puleeno Nguyen <puleeno@gmail.com>
2022-12-12 15:20:01 +0700

    Support save video meta

--- includes/Popcorn/Admin/Metabox/EditVideoInfo.php
+++ includes/Popcorn/Admin/Metabox/EditVideoInfo.php
@@ -1,7 +1,9 @@
 <?php
 namespace Ramphor\Popcorn\Admin\Metabox;
 
+use Ramphor\Popcorn\Platform\Common;
 use Ramphor\Popcorn\Video;
+use Ramphor_Popcorn;
 
 class EditVideoInfo
 {
@@ -25,20 +27,35 @@ class EditVideoInfo
         ?>
         <p>
             <label for=""><?php echo __('Embed code', 'ramphor_popcorn'); ?></label>
-            <textarea name="" id="ramphor-popcorn-video-embded" class="widefat"><?php echo $video->getEmbedCode(); ?></textarea>
+            <textarea name="<?php echo Common::EMBED_META_KEY; ?>" id="ramphor-popcorn-video-embded" class="widefat"><?php echo $video->getEmbedCode(); ?></textarea>
         </p>
         <p>
             <label for=""><?php echo __('Preview URL', 'ramphor_popcorn'); ?></label>
-            <input type="text" class="widefat" name="" value="<?php echo $video->getPreviewVideoUrl(); ?>" />
+            <input type="text" class="widefat" name="<?php echo Common::VIDEO_PREVIEW_URL_META_KEY; ?>" value="<?php echo $video->getPreviewVideoUrl(); ?>" />
         </p>
         <p>
             <label for=""><?php echo __('Duration', 'ramphor_popcorn'); ?></label>
-            <input type="text" class="widefat" name="" value="<?php echo $video->getDuration(); ?>" />
+            <input type="text" class="widefat" name="<?php echo Common::DURATION_META_KEY; ?>" value="<?php echo $video->getDuration(); ?>" />
         </p>
         <?php
     }
 
     public function saveInfo($post)
     {
+        if ($post->post_type !== Ramphor_Popcorn::POST_TYPE) {
+            return;
+        }
+        $videoMetaKeys = [
+            Common::EMBED_META_KEY,
+            Common::DURATION_META_KEY,
+            Common::VIDEO_PREVIEW_URL_META_KEY
+        ];
+        foreach($videoMetaKeys as $metaKey) {
+            if (empty($_POST[$metaKey])) {
+                delete_post_meta($post->ID, $metaKey);
+            } else {
+                update_post_meta($metaKey, $metaKey, $_POST[$metaKey]);
+            }
+        }
     }
 }
